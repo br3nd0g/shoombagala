181b054e53f4375d7e2a535d65e25a788f588f66
Brendan Corcoran <96981774+br3nd0g@users.noreply.github.com>
2022-12-24 20:25:50 +0000

    added db interaction functions and triggers

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const { Client, Events, GatewayIntentBits, EmbedBuilder, AttachmentBuilder, ActivityType } = require('discord.js');
 const { send } = require('process');
 const { rollMonster, getMonsterList } = require('./monsterdata')
-const { checkIfClaimed, addClaim, getUserClaims } = require('./userdata')
+const { checkIfClaimed, addClaim, getUserClaims, checkRolls, resetClaimsAvailable, checkCanClaim } = require('./userdata')
+fs = require('fs');
+
 
 const client = new Client({ intents: [
     GatewayIntentBits.DirectMessages,
@@ -27,6 +29,31 @@ function makeHelpString(){
     return helpS
 }
 
+async function claimTimerCheck(){
+
+    var dateObj = new Date();
+    var hours = dateObj.getHours(); 
+    var data;
+
+    try {
+        data = await fs.readFileSync('./lastReset.txt', 'utf8');
+    } catch (err) {
+        console.error(err);
+    }
+    
+    if (hours.toString() !== data){
+
+        await resetClaimsAvailable()
+
+        try {
+            await fs.writeFileSync('./lastReset.txt', hours.toString());
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+
 const helpString = makeHelpString()
 
 var monsterList;
@@ -42,12 +69,11 @@ async function sendMonster(message){
     const name = monsterDetails.chosMonster;
     var rarity = monsterDetails.rar;
     const imageURL = monsterDetails.photoUrl;
-    var possibleUserID;
 
     //check here if monster has been rolled in guild, and if so, put claimed on footer and who by- also prevent being able to claim it with reaction.(and remove react to claim message! (put the stuff in the .then() as an if))
-    const claimCheck = checkIfClaimed(name, rarity, message.guildId)
+    const claimCheck = await checkIfClaimed(name, rarity, message.guildId)
 
-    const monsterembed = new EmbedBuilder()
+    var monsterembed = new EmbedBuilder()
     .setTitle(name)
     .setColor(0x1c6b24)
     .addFields(
@@ -56,21 +82,35 @@ async function sendMonster(message){
     .setImage(imageURL)
 
     if(claimCheck.claimed == true){
-        //var userWhoClaimed = client.users.cache.get(claimCheck.userid);
 
-        //console.log(user.username)
-        //monsterembed = EmbedBuilder.from(monsterembed).setFooter({ text: `Claimed by ${user.tag}`, iconURL: user.displayAvatarURL() });
+        var userWhoClaimed = message.guild.members.cache.find(user => user.id === claimCheck.userid);
+        monsterembed = EmbedBuilder.from(monsterembed).setFooter({ text: `Owned by ${userWhoClaimed.displayName}`, iconURL: userWhoClaimed.displayAvatarURL() });
     }
 
     message.channel.send({ embeds: [monsterembed]}).then((msg) =>{
+        if(claimCheck.claimed == false){
 
-        const collector = msg.createReactionCollector({ time: 30000 });
+            const collector = msg.createReactionCollector({ time: 30000 });
 
-        collector.on('collect', (reaction, user) => {
-            const editedEmbed = EmbedBuilder.from(monsterembed).setFooter({ text: `Claimed by ${user.tag}`, iconURL: user.displayAvatarURL() });
-            msg.edit({ embeds: [editedEmbed] });
-        });
+            collector.on('collect', (reaction, user) => {
+                handleReaction(collector, user, msg, monsterembed, name, rarity)
+            });
+
+        }
     });
+}
+
+async function handleReaction(collector, user, msg, embed, monName, monRarity){
+    const canUserClaim = await checkCanClaim(user.id, msg.guildId)
+    if(canUserClaim === true){
+        await addClaim(monName, monRarity, user.id, msg.guildId)
+        const editedEmbed = EmbedBuilder.from(embed).setFooter({ text: `Claimed by ${user.tag}`, iconURL: user.displayAvatarURL() });
+        msg.edit({ embeds: [editedEmbed] });
+        collector.stop()
+    }
+    else{
+        msg.channel.send(`**${user.username}**, you have used your current claim.`)
+    }
 }
 
 client.on('ready', () => {
@@ -84,6 +124,8 @@ client.on('ready', () => {
 
 client.on('messageCreate', async (message) => {
 
+    await claimTimerCheck()
+
     if(message.content.startsWith(commandPrefix)){
 
         const msgCon = message.content.substring(commandPrefix.length)
@@ -94,8 +136,12 @@ client.on('messageCreate', async (message) => {
 
         if(msgCon.startsWith("monster")){
 
-            sendMonster(message)
-            
+
+            rolls = await checkRolls(message.author.id, message.guildId)
+
+            if (rolls === 0){
+                message.channel.send("You have no rolls left.")
+            }else{sendMonster(message)}
         }
         
         if(msgCon.startsWith("evil")){
@@ -119,6 +165,10 @@ client.on('messageCreate', async (message) => {
 
 async function startBot(){
     monsterList = await getMonsterList()
+    const bookwormindex = monsterList.indexOf("Boo'qwurm");
+    monsterList.splice(bookwormindex, 1);
+    const blowtindex = monsterList.indexOf("Blow't");
+    monsterList.splice(blowtindex, 1);
     setTimeout(() => {
         //console.log(monsterList);
         client.login(process.env.clToken); }, 2000)
--- userdata.js
+++ userdata.js
@@ -1,36 +1,140 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./userdata.db');
+function selectDbPromise(db, query) {
 
-function checkIfClaimed(monster, rarity, guildid){
-    
-    monster = "Maw"
-    rarity = "Common"
-    var claimed = false
+    return new Promise((resolve, reject) => {
+
+        db.all(query, [], (err, rows) => {
+
+            if(err) {
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+function editDbPromise(db, query) {
+
+    return new Promise((resolve, reject) => {
+
+        db.run(query, [], (err, rows) => {
+
+            if(err) {
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+async function checkIfClaimed(monster, rarity, guildid){
+
+    let db = new sqlite3.Database('./userdata.db');
+
     var userid;
+    var claimed = false;
 
-    const sqlQuery = `SELECT userID FROM Claims WHERE monsterName = '${monster}' AND guildID = '${guildid}' AND Rarity = '${rarity}';    `
+    const sqlQuery = `SELECT userID FROM Claims WHERE monsterName = '${monster}' AND guildID = '${guildid}' AND Rarity = '${rarity}';`
 
-    db.all(sqlQuery, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            claimed = True
-            userid = row
-        });
+    const rows = await selectDbPromise(db, sqlQuery)
+
+    rows.forEach((row) => {
+        claimed = true
+        userid = row.userID
     });
+    if(rows.length === 0){
+        //console.log("No Claim on " + monster)
+    }
+
+    db.close()
 
     return { claimed, userid }
 }
 
-function addClaim(){
-    //take guild, userid, monster.
-    //insert guildid as guildid and so on
+async function addClaim(monster, rarity, userID, guildID){
+
+    let db = new sqlite3.Database('./userdata.db');
+
+    const sqlQuery = `INSERT INTO Claims (userID, guildID, monsterName, Rarity) VALUES('${userID}', '${guildID}', '${monster}', '${rarity}');`
+
+    await editDbPromise(db, sqlQuery)
+
+    const useClaimQuery = `UPDATE ClaimTimers SET claimsAvailable = 0 WHERE guildID = '${guildID}' AND userID = '${userID}';`
+
+    await editDbPromise(db, useClaimQuery)
+
+    db.close()
 }
 
-function getUserClaims(){
+async function checkRolls(userID, guildID){
+
+    let db = new sqlite3.Database('./userdata.db');
+
+    var rolls;
+    const sqlQuery = `SELECT rollsAvailable FROM ClaimTimers WHERE userID = '${userID}' AND guildID = '${guildID}';`
+
+    const rows = await selectDbPromise(db, sqlQuery)
+
+    rows.forEach((row) => {
+        rolls = row.rollsAvailable
+    });
+
+    //console.log("LENGTH OF ROWS IS " + rows.length)
+
+    if(rows.length === 0){
+        const addUserQuery = `INSERT INTO ClaimTimers (userID, guildID) VALUES('${userID}', '${guildID}');`
+
+        await editDbPromise(db, addUserQuery)
+
+        rolls = 9
+    }
+
+    if (rolls > 0){
+        const sqlUpdateQuery = `UPDATE ClaimTimers SET rollsAvailable = ${rolls -= 1} WHERE userID = '${userID}' AND guildID = '${guildID}';`
+        await editDbPromise(db, sqlUpdateQuery)
+    }
+
+    db.close()
+
+    //console.log(userID + " can roll " + rolls + " more times")
+
+    return rolls
+
+}
+
+async function resetClaimsAvailable(){
+    let db = new sqlite3.Database('./userdata.db');
+
+    const sqlQuery = `UPDATE ClaimTimers SET rollsAvailable = 8, claimsAvailable = 1;`
+
+    await editDbPromise(db, sqlQuery)
+
+    db.close()
+}
+
+function getUserClaims(userID, guildID){
     //query all monsters by user in guild
 }
 
-module.exports = { checkIfClaimed, addClaim, getUserClaims }
+async function checkCanClaim(userID, guildID){
+    let db = new sqlite3.Database('./userdata.db');
+
+    const sqlQuery = `SELECT claimsAvailable FROM ClaimTimers WHERE guildID = '${guildID}' AND userID = '${userID}';`
+
+    const rows = await selectDbPromise(db, sqlQuery)
+    var claimsAvailable;
+    var canClaim = true
+
+    rows.forEach((row) => {
+        claimsAvailable = row.claimsAvailable
+    });
+
+    if(claimsAvailable === 0){canClaim = false}
+
+    db.close()
+
+    return canClaim
+}
+
+module.exports = { checkIfClaimed, addClaim, getUserClaims, checkRolls, resetClaimsAvailable, checkCanClaim }
